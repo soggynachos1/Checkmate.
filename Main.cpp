7623eb29ba597a5c3d0d8caa36ec5576fd8e9ea7
Angel Carrillo <147093277+soggynachos1@users.noreply.github.com>
2025-04-04 19:57:13 -0600

    Update Main.cpp
    i added to the main.cpp file to make it orange

--- Main.cpp
+++ Main.cpp
@@ -1,45 +1,40 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-//enum directions { down, right, up, left };
-int main()
-{
-   /* int i;
-    unsigned int width = 640;
-    unsigned int height = 360;
-    */
-    sf::RenderWindow window(sf::VideoMode({ 2500,1500 }), "SFML works!");
-   /* sf::Texture texture;
-    if (!texture.loadFromFile("Sprite.hpp"))
-    {
-        return -1; // Error loading the texture
-    }
 
-    sf::Sprite sprite(texture);
-    sf::IntRect dir[4];
+int main() {
 
-    for (i = 0; i < 4; ++i)
-    {
-        dir[i] = sf::IntRect({ {32 * i, 0}, {32, 32} });
-    }
+	
+	unsigned int width = 2000;
+	unsigned int height = 1500;
+	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({ width,height }), "Chess Puzzle Game");
+	
+	while (window->isOpen()) 
+	{
+		while (const std::optional event = window->pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+			{
+				window->close();
+			}
+			else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+			{
+				if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+				{
+					window->close();
+				}
+			}
+		}
+		//Render
+		window->clear(sf::Color(0xFF8800FF));
 
-    sprite.setTextureRect(dir[down]);
-    sprite.setOrigin({ 16,16 });
-    sprite.setPosition({ width / 2.0f, height / 2.0f });
-    */
+		//Drawing 
 
-    while (window.isOpen())
-    { 
-       
-       while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-        
-        window.clear();
-        //drawing purposes
-      //  window.draw(sprite);
-        window.display();
-    }
+		window->display();
+
+	}
+
+	delete window;
+
+	return 0;
 }
