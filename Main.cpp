0ba2faadc95a98137693665527cf81d58c6e5286
Angel Carrillo <147093277+soggynachos1@users.noreply.github.com>
2025-04-05 09:59:53 -0600

    Update Main.cpp
    i added some text

--- Main.cpp
+++ Main.cpp
@@ -2,13 +2,32 @@
 #include <SFML/Graphics.hpp>
 
 
+
+
+
 int main() {
 
-	
+
 	unsigned int width = 2000;
 	unsigned int height = 1500;
 	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({ width,height }), "Chess Puzzle Game");
-	
+	window->setFramerateLimit(60);
+
+	sf::Font font("Fonts\\jersey10.ttf");
+
+	sf::Text text1(font); 
+	text1.setString("Chess Puzzle Game");
+	text1.setCharacterSize(100); 
+	text1.setFillColor(sf::Color::Green);
+	text1.setPosition({ 650,400 });
+
+
+	sf::Text text2(font);
+	text2.setCharacterSize(60);
+	text2.setString("Press Enter To Play");
+	text2.setFillColor(sf::Color::Green);
+	text2.setPosition({ 750,600 });
+
 	while (window->isOpen()) 
 	{
 		while (const std::optional event = window->pollEvent())
@@ -25,11 +44,17 @@ int main() {
 				}
 			}
 		}
+	
+
+
 		//Render
-		window->clear(sf::Color(0xFF8800FF));
+		window->clear();
 
 		//Drawing 
+		window->draw(text1);
+		window->draw(text2);
 
+		//Display
 		window->display();
 
 	}
