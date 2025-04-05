cc4af09227b80b4492ecbda82dc29097111e311c
Angel Carrillo <147093277+soggynachos1@users.noreply.github.com>
2025-04-05 12:57:08 -0600

    Update Main.cpp
    added sound and more text

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-
+#include <SFML/Audio.hpp>
 
 
 
@@ -10,25 +10,60 @@ int main() {
 
 	unsigned int width = 2000;
 	unsigned int height = 1500;
-	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({ width,height }), "Chess Puzzle Game");
+	sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode({ width,height }), "Chess Puzzle Game");
 	window->setFramerateLimit(60);
+	
+
+
+	sf::SoundBuffer buffer;
+	if (!buffer.loadFromFile("media\\background2.wav")) {
+		std::cerr << "Error loading sound file" << std::endl;
+		return -1;
+	}
+	sf::Sound sound(buffer);
+	sound.setPitch(1.0f);
+	sound.setLooping(true);
+	sound.setVolume(30.f);
+	
+	while (sound.getStatus() == sf::Sound::Status::Playing)
+	{
+		sf::sleep(sf::milliseconds(100));
+	}
+	sound.play();
+
 
 	sf::Font font("Fonts\\jersey10.ttf");
 
 	sf::Text text1(font); 
 	text1.setString("Chess Puzzle Game");
-	text1.setCharacterSize(100); 
-	text1.setFillColor(sf::Color::Green);
-	text1.setPosition({ 650,400 });
+	text1.setCharacterSize(175); 
+	text1.setFillColor(sf::Color::Black);
+	text1.setPosition({ 450,400 });
 
 
 	sf::Text text2(font);
-	text2.setCharacterSize(60);
+	text2.setCharacterSize(100);
 	text2.setString("Press Enter To Play");
-	text2.setFillColor(sf::Color::Green);
-	text2.setPosition({ 750,600 });
+	text2.setFillColor(sf::Color::Black);
+	text2.setPosition({ 650,650 });
 
-	while (window->isOpen()) 
+	sf::Text text3(font);
+	text3.setCharacterSize(100);
+	text3.setString("Quit");
+	text3.setFillColor(sf::Color::Black);
+	text3.setPosition({ 900,800 });
+
+	
+	sf::Texture texture("Sprites\\queen1.png");
+
+	sf::Sprite sprite(texture);
+	sprite.setOrigin({ 32,32 });
+	sprite.setPosition({ 350,200 });
+	sprite.scale(sf::Vector2f(4.0, 4.0));
+
+
+	
+	while (window->isOpen())
 	{
 		while (const std::optional event = window->pollEvent())
 		{
@@ -44,22 +79,31 @@ int main() {
 				}
 			}
 		}
+
+
 	
 
 
 		//Render
-		window->clear();
-
+		window->clear(sf::Color(245, 245, 220));
+		
+        
 		//Drawing 
 		window->draw(text1);
 		window->draw(text2);
+		window->draw(text3);
+		window->draw(sprite);
 
 		//Display
 		window->display();
+	
 
 	}
 
+	
 	delete window;
+
+
 
 	return 0;
 }
